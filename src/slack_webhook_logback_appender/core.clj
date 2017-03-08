e1ebca8dc75a1702f15d5eebfe6b7e624bb1f085
Tsutomu YANO <tsutomu.yano@uzabase.com>
2017-03-08 11:33:32 +0900

    Added an implementation of SlackAppender

--- src/slack_webhook_logback_appender/core.clj
+++
@@ -1,6 +0,0 @@
-(ns slack-webhook-logback-appender.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- test/slack_webhook_logback_appender/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns slack-webhook-logback-appender.core-test
-  (:require [clojure.test :refer :all]
-            [slack-webhook-logback-appender.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
