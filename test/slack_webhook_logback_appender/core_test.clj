c8232ffb94d7638bbded87774f80a42eea079de6
Tsutomu YANO <tsutomu.yano@uzabase.com>
2017-03-08 11:20:17 +0900

    first commit

---
+++ test/slack_webhook_logback_appender/core_test.clj
@@ -0,0 +1,7 @@
+(ns slack-webhook-logback-appender.core-test
+  (:require [clojure.test :refer :all]
+            [slack-webhook-logback-appender.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
